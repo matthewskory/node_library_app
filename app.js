55792bd57bb630825f82b90cc46505a90ea54753
Matthew Kory <matthewskory@gmail.com>
2016-09-02 12:04:31 -0400

    added routes for books and single books

--- app.js
+++ app.js
@@ -1,15 +1,59 @@
 var express = require('express');
-
 var app = express();
 
 var port = process.env.PORT ||5000;
+var bookRouter = express.Router();
 
 app.use(express.static('public'));
-app.set('views', 'src/views');
-app.set('view engine', 'ejs')
+app.set('views', './src/views');
+app.set('view engine', 'ejs'); //sets view engine to ejs
+
+var books =[
+{
+	title: "Sevenes",
+	author: "Neal Stephenson"
+},
+{
+	title: "Wool",
+	author: "Hugh Howey"
+}
+];
+
+bookRouter.route('/')
+	.get(function(req, res){
+		res.render('books',{
+		title: "Books",
+		nav: [{
+			Link:'/Books',
+			Text: 'Books'
+		},
+		{
+			Link: '/Authors',
+			Text:'Authors'
+		}],
+		books: books
+	});
+});
+
+bookRouter.route('/single')
+	.get(function(req, res){
+		res.send("hello single book");
+	});
+
+app.use('/Books', bookRouter);
 
 app.get('/', function(req, res){
-	res.render('index', list=['a', 'b']);
+	res.render('index',{
+		title: "Books",
+		nav: [{
+			Link:'/Books',
+			Text: 'Books'
+		},
+		{
+			Link: '/Authors',
+			Text:'Authors'
+		}]
+	});
 });
 
 app.get('/books', function(req, res){
